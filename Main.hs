6114278e112d4e330eea9e15c24cd41298d78381
minamiyama <ri-minamiyama@kumahira-safe.co.jp>
2024-01-11 15:45:15 +0900

    構文解析失敗時にも文字列を返すように変更

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import AST
 import Parser
 import Unification
 import Constraints
@@ -7,10 +8,15 @@ import Apply
 import ToString
 import System.Environment (getArgs)
 
+parseErrorSig :: Signature
+parseErrorSig = Signature "error" Colon (Single (JustTypeName (TypeName "parsing failed")))
+
 main :: IO()
 main = do input <- getArgs
           let sigs = map parse2signature input
-          let unified = unifySignatures sigs
-          let constraints = getConstraints [] sigs
-          let output = signature2str $ applyConstrainsSignature constraints unified
-          print output
+          if parseErrorSig `elem` sigs
+              then print $ signature2str parseErrorSig
+              else do let unified = unifySignatures sigs
+                      let constraints = getConstraints [] sigs
+                      let output = signature2str $ applyConstrainsSignature constraints unified
+                      print output
